074b9dd0fe011b673f77d8b99b53ba3975e26819
Konov Aleksey <konov.alexey@gmail.com>
2024-05-20 18:52:06 +0300

    Create API methods by view-class (high-level)

--- kittygram/urls.py
+++ kittygram/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from cats.views import APICat
+from cats.views import CatDetail, CatList
 
 urlpatterns = [
-   path('cats/', APICat.as_view()),
+   path('cats/', CatList.as_view()),
+   path('cats/<int:pk>/', CatDetail.as_view()),
 ]
